4e242a3680d99d371ff87a45e2cfd81fbe9cdb0d
patrick <tasniim_bari@yahoo.com>
2021-12-27 15:04:25 +0000

    sample database created to populate table

---
+++ data.js
@@ -0,0 +1,7 @@
+export function data(){[
+    {id:1, item:'rice noodles',store:'aldi',weight:'500g', price: "£2.00"},
+    {id:2, item:'Microwave rice',store:'aldi',weight:'500g', price: "£0.70"},
+    {id:3, item:'chicken breast',store:'Lidl',weight:'1kg', price: "£9.90"},
+    {id:4, item:'fried rice paste',store:'Duc Tien',weight:'500g', price: "£1.00"},
+    {id:5, item:'sage',store:'aldi',weight:'75g', price: "£0.15"}
+]}
